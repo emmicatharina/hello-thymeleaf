d837219ae9798fff892fceb09eb1fb67bd4ec946
emmicatharina <43492887+emmicatharina@users.noreply.github.com>
2021-09-06 22:49:11 +0300

    Add adding friends to list

--- src/main/java/fi/server2021/ModelView/web/FriendController.java
+++ src/main/java/fi/server2021/ModelView/web/FriendController.java
@@ -1,16 +1,27 @@
 package fi.server2021.ModelView.web;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FriendController {
+	
+	ArrayList<String> friends = new ArrayList<>();
 
 	@RequestMapping(value = "/index", method = RequestMethod.GET)
-	public String friendForm(Model model) {
-		
+	public String friendForm(@RequestParam(name = "friend", required = false) String friend, Model model) {
+		if (friend != null) {
+			friends.add(friend);
+		}
+		model.addAttribute("friend", friend);
+		model.addAttribute("friends", friends);
 		return "friendlist";
 	}
+	
+	
 }
